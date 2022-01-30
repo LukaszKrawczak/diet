6e788aff43fc36415ade2a04cf8082ca10e4cfd9
Lukasz Krawczak <krawczak.lukasz@gmail.com>
2022-01-30 18:13:24 +0100

    Adding SpringApplicationBuilder

--- src/main/java/com/demo/diet/DietApplication.java
+++ src/main/java/com/demo/diet/DietApplication.java
@@ -2,6 +2,7 @@ package com.demo.diet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +16,12 @@ public class DietApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(DietApplication.class, args);
         System.out.println("tu kornelia");
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder
+                                                         application) {
+        return application.sources(DietApplication.class);
     }
 
     @RequestMapping(value = "/")
