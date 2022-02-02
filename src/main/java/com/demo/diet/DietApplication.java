d58a51a63e1fb730920db6e13d0a69734a0217e7
Lukasz Krawczak <krawczak.lukasz@gmail.com>
2022-02-02 23:12:32 +0100

    changing text on welcome page v2

--- src/main/java/com/demo/diet/DietApplication.java
+++ src/main/java/com/demo/diet/DietApplication.java
@@ -25,7 +25,7 @@ public class DietApplication extends SpringBootServletInitializer {
 
     @RequestMapping(value = "/")
     public String hello() {
-        return "Hello world, witam po update ;) v1";
+        return "Hello world, witam po update ;) kolejny test pipeline git -> jenkins -> tomcat";
     }
 }
 
