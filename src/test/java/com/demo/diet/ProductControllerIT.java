8e636c9a428e27665c08bfa89fc20d6701b27177
LukaszKrawczakSP <76959194+LukaszKrawczakSP@users.noreply.github.com>
2023-11-03 14:26:02 +0100

    add TestContainers integration, add mysql db init scripts, add IT tests, removing bad impl tests (#7)
    * Add spring-boot-testcontainers artifactId
    
    * add db init scripts, add mysql cfg for testcontainer
    
    * add EOF
    
    * removing WebMockTest bad impl class
    
    * adding integration tests to ProductControllerIT
    
    * removing ProductDtoTest
    
    * removing UserController

---
+++ src/test/java/com/demo/diet/ProductControllerIT.java
@@ -0,0 +1,153 @@
+package com.demo.diet;
+
+import com.demo.diet.model.NutrientsDto;
+import com.demo.diet.model.ProductDto;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ProductControllerIT extends BaseIT {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    @Sql({"/scripts/schema.sql", "/data/clearAll.sql", "/data/testData.sql"})
+    public void shouldReturnProductWhenExistsInDbTables() throws Exception {
+        // given
+        ProductDto expected = ProductDto.builder()
+                .name("Test product")
+                .nutrientsDto(
+                        NutrientsDto.builder()
+                                .proteins(123)
+                                .carbohydrates(123)
+                                .fats(123)
+                                .build()
+                )
+                .build();
+        // when
+        MvcResult mvcResult = mockMvc.perform(get("/api/v1/products/1"))
+                .andExpect(status().isOk())
+                .andDo(MockMvcResultHandlers.print())
+                .andReturn();
+        // then
+        String content = mvcResult.getResponse().getContentAsString();
+        assertEquals(asJsonString(expected), content);
+    }
+
+    @Test
+    @Sql({"/scripts/schema.sql", "/data/clearAll.sql", "/data/testData.sql"})
+    public void shouldReturnSecondProductWithDifferentIdWhenExistsInDbTables() throws Exception {
+        // given
+        ProductDto expected = ProductDto.builder()
+                .name("Test product 2")
+                .nutrientsDto(
+                        NutrientsDto.builder()
+                                .proteins(123)
+                                .carbohydrates(123)
+                                .fats(123)
+                                .build()
+                )
+                .build();
+        // when
+        MvcResult mvcResult = mockMvc.perform(get("/api/v1/products/2"))
+                .andExpect(status().isOk())
+                .andDo(MockMvcResultHandlers.print())
+                .andReturn();
+        // then
+        String content = mvcResult.getResponse().getContentAsString();
+        assertEquals(asJsonString(expected), content);
+    }
+
+    @Test
+    @Sql({"/scripts/schema.sql", "/data/clearAll.sql", "/data/testData.sql"})
+    public void shouldReturnNotFoundWhenProductNotExistsInDbTables() throws Exception {
+        // given 🤣
+        // when
+        MvcResult mvcResult = mockMvc.perform(get("/api/v1/products/999"))
+                .andExpect(status().isNotFound())
+                .andDo(MockMvcResultHandlers.print())
+                .andReturn();
+
+        // then
+        String content = mvcResult.getResponse().getContentAsString();
+        assertEquals("", content);
+    }
+
+    @Test
+    @Sql({"/scripts/schema.sql", "/data/clearAll.sql", "/data/testData.sql"})
+    public void shouldReturnAlreadyExistsWhenTryingToAddNewProduct() throws Exception {
+        // given
+        ProductDto productDto = ProductDto.builder()
+                .name("Test product")
+                .nutrientsDto(
+                        NutrientsDto.builder()
+                                .proteins(123)
+                                .carbohydrates(123)
+                                .fats(123)
+                                .build()
+                )
+                .build();
+
+        // when
+        MvcResult mvcResult = mockMvc.perform(post("/api/v1/products/add")
+                        .content(asJsonString(productDto))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isConflict())
+                .andDo(MockMvcResultHandlers.print())
+                .andReturn();
+
+        // then
+        assertEquals("Product with name 'Test product' already exists", mvcResult.getResponse().getContentAsString());
+    }
+
+    @Test
+    @Sql({"/scripts/schema.sql", "/data/clearAll.sql"})
+    public void shouldAddProductToDbTables() throws Exception {
+        // given
+        ProductDto productDto = ProductDto.builder()
+                .name("Test product")
+                .nutrientsDto(
+                        NutrientsDto.builder()
+                                .proteins(123)
+                                .carbohydrates(123)
+                                .fats(123)
+                                .build()
+                )
+                .build();
+
+        // when
+        MvcResult mvcResult = mockMvc.perform(post("/api/v1/products/add")
+                        .content(asJsonString(productDto))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated())
+                .andDo(MockMvcResultHandlers.print())
+                .andReturn();
+
+        // then
+        String content = mvcResult.getResponse().getContentAsString();
+        assertEquals(asJsonString(productDto), content);
+    }
+
+    public static String asJsonString(final Object obj) {
+        try {
+            return new ObjectMapper().writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
